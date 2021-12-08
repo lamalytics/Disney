37bf9db60363db77bc1fcac18664a5ba23f00793
lamalytics <95007715+lamalytics@users.noreply.github.com>
2021-12-07 20:51:00 -0800

    Update Disney_Obs.R

--- Disney_Obs.R
+++ Disney_Obs.R
@@ -1,6 +1,6 @@
 pacman::p_load(data.table, qs, stringr, tidyverse, googlesheets4, ggplot2, 
                lubridate, reshape2, openxlsx, scales, googledrive, zoo,
-               forecast)
+               forecast, rlist)
 
 disney_raw <- read.csv("disney_plus_titles.csv")
 
@@ -41,19 +41,73 @@ type_tally %>%
 country_availability <- disney %>% 
   filter(grepl("Germany", country) | !grepl("", country))
 
-countries <- c("United States", "United Kingdom", "Austrailia", "Germany", "France",
-               "South Korea", "Japan", "Canada", "Mexico")
+countries <- c("United States", "United Kingdom", "Australia", "Germany", "France",
+               "South Korea", "Japan", "Canada", "Mexico", "Spain", "Hungary", "India",
+               "Singapore", "Ireland", "Brazil")
+
 
 # mapping of each country and how many titles are offered on disney
 # currently displays indicies, will need to change to labels
-map_df(countries, 
-       ~ disney %>% 
-         filter(grepl(.x, country, ignore.case=TRUE)) %>% 
-         count(), 
-       .id="Country")
 
+
+# get a unique country list
+country_list <- unlist(str_split(disney$country, ","))
+country_list <- unique(trimws(country_list))
+countries <- country_list
+print(country_list)
+
+# get total titles
+total_titles <- sum(nrow(disney))
+# get mapping for country counts
+disney_countries <- map_df(countries, ~ disney %>% 
+         filter(grepl(.x, country, ignore.case=TRUE)) %>% 
+         count(), .id="Country") %>% 
+  mutate(Country_Name = countries[as.numeric(Country)],
+         Number_of_titles = n,
+         Percentage_of_titles = Number_of_titles/total_titles)
+
+disney_countries <- disney_countries %>% 
+  select(Country_Name, Number_of_titles, Percentage_of_titles) %>% 
+  filter(Country_Name != "") %>% 
+  arrange(desc(Number_of_titles))
+
+# column plot of titles by country
+disney_countries %>% 
+  arrange(Number_of_titles) %>% 
+  top_n(10, Number_of_titles) %>% 
+  ggplot(aes(x = reorder(Country_Name, -Percentage_of_titles), y = Percentage_of_titles), color = Country_Name) +
+  geom_col() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  labs(x = "Country", y = "% Share of Disney+ Titles") +
+  annotate("text", x = 1.3, y = 0.9, label = "1147 titles!")
   
 # Network analysis of Actors / Directors and find interesting insights
+# unpack list similar to titles
+director_list <- unlist(str_split(disney$director, ","))
+director_list <- unique(trimws(director_list))
+print(director_list)
+
+disney_director <- map_df(director_list, ~ disney %>% 
+                            filter(grepl(.x, director, ignore.case=TRUE)) %>% 
+                            count(), .id="director") %>% 
+  mutate(Director_Name = director_list[as.numeric(director)],
+         Number_of_titles = n,
+         Percentage_of_titles = Number_of_titles/total_titles) %>% 
+  arrange(desc(Number_of_titles))
+
+actor_list <- unlist(str_split(disney$cast, ","))
+actor_list <- unique(trimws(actor_list))
+print(actor_list)
+
+# will need to correct due to NA and possible misattribution
+disney_actor <- map_df(actor_list, ~ disney %>% 
+                            filter(grepl(.x, cast, ignore.case=TRUE)) %>% 
+                            count(), .id="actor") %>% 
+  mutate(Actor_Name = director_list[as.numeric(actor)],
+         Number_of_titles = n,
+         Percentage_of_titles = Number_of_titles/total_titles) %>% 
+  arrange(desc(Number_of_titles))
+
 
 # Identifying similar content by matching text-based features
 
